eaa75a18ac05c18047081bba9fc2988bf40b64de
yordycg <yordy.carmona8@gmail.com>
2024-10-21 21:35:56 -0300

    update styles for LOGIN and change some code for JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,48 +1,37 @@
 "use strict";
-// ! Variables globales
-const modal = document.getElementById("modal--login");
-const openModalBtn = document.getElementById("btn--open--modal");
-const closeModalBtn = document.getElementById("btn--close--modal");
+// Variables globales ------------------------------------------------------------------------------
+
+// VALIDAR UN USUARIO EN LOGIN ---------------------------------------------------------------------
 const formLogin = document.getElementById("login--form");
 
-// ! Obtener datos del formulario de login
-formLogin.addEventListener("submit", function (event) {
+formLogin.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  // obtner los datos del formulario
-  const username = document.getElementById("username").value;
-  const password = document.getElementById("password").value;
-  console.log(`username: ${username}`);
-  console.log(`password: ${password}`);
+  // obtener los datos del formulario
+  const usernameLogin = document.getElementById("usernameLogin").value;
+  const passwordLogin = document.getElementById("passwordLogin").value;
 
   // validar los datos con una cuenta de usuario "falsa"
-  if (username == "admin10" && password == "12345678") {
+  if (usernameLogin == "admin" && passwordLogin == "1234") {
     window.location.href = "../assets/public/inventario.html";
   } else {
     alert("Datos ingresados con corresponden!");
   }
 });
 
-// ! Modal - login
-document.addEventListener("DOMContentLoaded", function () {
-  // const modal = document.getElementById("modal--login");
-  // const openModalBtn = document.getElementById("btn--open--modal");
-  // const closeModalBtn = document.getElementById("btn--close--modal");
+// MODAL LOGIN -------------------------------------------------------------------------------------
+const modal = document.getElementById("modal--login");
+const openModalBtn = document.getElementById("btn--open--modal");
+const closeModalBtn = document.getElementById("btn--close--modal");
 
+document.addEventListener("DOMContentLoaded", () => {
   // Abrir el modal
-  openModalBtn.addEventListener("click", function () {
+  openModalBtn.addEventListener("click", () => {
     modal.showModal(); // Método nativo para abrir el <dialog>
   });
 
   // Cerrar el modal
-  closeModalBtn.addEventListener("click", function () {
+  closeModalBtn.addEventListener("click", () => {
     modal.close(); // Método nativo para cerrar el <dialog>
-  });
-
-  // Cerrar el modal al hacer clic fuera del contenido
-  modal.addEventListener("click", function (event) {
-    if (event.target === modal) {
-      modal.close();
-    }
   });
 });
