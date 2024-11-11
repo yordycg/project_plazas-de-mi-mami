eb6f048dd8c9f46efe0941df1a489424d4387cee
yordycg <yordy.carmona8@gmail.com>
2024-11-11 17:39:56 -0300

    update: solution for sidebar styles

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,43 +1,46 @@
 export default function toggleSidebar() {
-  const toggleBtn = document.getElementById('toggle-btn');
-  const sidebar = document.querySelector('.sidebar--section');
-  const footer = document.querySelector('.footer--section');
-  const footerSpan = document.querySelector('.footer--section span');
-  const searchInput = document.querySelector('.search--input');
-  const searchBar = document.querySelector('.search--bar');
-  const searchIcon = document.querySelector('.search--icon');
+  const toggleBtn = document.getElementById("toggle-btn");
+  const sidebar = document.querySelector(".sidebar--section");
+  const footer = document.querySelector(".footer--section");
+  const footerSpan = document.querySelector(".footer--section span");
+  const searchInput = document.querySelector(".search input");
+  const searchIcon = document.querySelector(".search--icon");
 
   // Abrir sidebar al hacer click en el input de búsqueda
-  searchBar?.addEventListener('click', () => {
-    if (sidebar.classList.contains('close')) {
-      sidebar.classList.remove('close');
-      footer.classList.remove('close');
-      toggleBtn.classList.remove('rotate');
-      searchInput.placeholder = '';
-      footerSpan.style.display = 'block';
+  searchInput?.addEventListener("click", () => {
+    if (sidebar.classList.contains("close")) {
+      sidebar.classList.remove("close");
+      footer.classList.remove("close");
+      toggleBtn.classList.remove("rotate");
+      searchInput.placeholder = "";
+      footerSpan.style.display = "inline";
     }
   });
 
   // Abrir sidebar al hacer click en el ícono de búsqueda
-  searchIcon?.addEventListener('click', () => {
-    if (sidebar.classList.contains('close')) {
-      sidebar.classList.remove('close');
-      footer.classList.remove('close');
-      toggleBtn.classList.remove('rotate');
-      searchInput.placeholder = '';
-      footerSpan.style.display = 'block';
+  searchIcon?.addEventListener("click", () => {
+    if (sidebar.classList.contains("close")) {
+      sidebar.classList.remove("close");
+      footer.classList.remove("close");
+      toggleBtn.classList.remove("rotate");
+      searchInput.placeholder = "";
+      footerSpan.style.display = "inline";
     }
   });
 
   // Abrir y cerrar sidebar con el botón
-  toggleBtn?.addEventListener('click', () => {
-    sidebar.classList.toggle('close');
-    footer.classList.toggle('close');
+  toggleBtn?.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+    footer.classList.toggle("close");
+    footerSpan.style.display = "none";
 
-    toggleBtn.classList.toggle('rotate');
+    toggleBtn.classList.toggle("rotate");
 
     // Agregar o quitar el placeholder
-    // if (sidebar.classList.contains('close')) searchInput.placeholder = 'Buscar...';
-    // else searchInput.placeholder = '     Buscar...';
+    if (sidebar.classList.contains("close")) {
+      searchInput.removeAttribute("placeholder");
+      // quitar padding de 5.4rem
+      searchInput.style.paddingLeft = "1.4rem";
+    }
   });
 }
